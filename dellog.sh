5dc23c33e6e161e0237d992a0b44f2b05b23a705
tranpduy87 <tranpduy87@gmail.com>
2022-03-01 12:16:38 +0900

    Update dellog.sh

--- dellog.sh
+++ dellog.sh
@@ -2,6 +2,6 @@
 # < this script > <begin loop> <end loop> <folder> <pattern of log>
 for x in {$1..$2}; do 
   cd $3$x/; 
-  find . -name "$4" -print0 | xargs -0 rm; 
-  find . -name "$5" -print0 | xargs -0 rm; 
+  find . -name "*err*" -print0 | xargs -0 rm; 
+  find . -name "*out*" -print0 | xargs -0 rm; 
 done
